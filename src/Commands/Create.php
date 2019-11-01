f57e3cbe3863e6f2e1f91c9a3b35ce629a21e50f
Logan Henson <logan@loganhenson.com>
2019-11-01 09:24:39 -0500

    --amend

--- src/Commands/Create.php
+++ src/Commands/Create.php
@@ -43,7 +43,7 @@ EOT;
         $elmJsonPath = $elmPath . '/elm.json';
 
         if (! File::isFile($elmJsonPath)) {
-            File::copy($elmJsonPath, __DIR__ . '/../Fixtures/elm.json');
+            File::copy(__DIR__ . '/../Fixtures/elm.json', $elmJsonPath);
         }
     }
 }
